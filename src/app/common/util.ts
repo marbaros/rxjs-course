dffdac8317457eb6b0ddc615cfe6bee92b53bd04
Mario Bajic <ba@haftpflichtkasse.de>
2023-12-29 12:29:10 +0100

    add teardown function for cancelation support

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -2,7 +2,12 @@ import { Observable } from "rxjs";
 
 export function createHttpObservable(url: string) {
   return new Observable((observer) => {
-    fetch(url)
+    const abortController = new AbortController();
+    const abortSignal = abortController.signal;
+
+    fetch(url, {
+      signal: abortSignal,
+    })
       .then((response) => {
         return response.json();
       })
@@ -13,5 +18,10 @@ export function createHttpObservable(url: string) {
       .catch((err) => {
         observer.error(err);
       });
+
+    // Rückgabe einer Teardown-Funktion, die beim Unsubscribe aufgerufen wird
+    return () => {
+      abortController.abort();
+    };
   });
 }
